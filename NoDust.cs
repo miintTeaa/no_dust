642611b7961ab042a1e97d7f6c55c1d20838a817
miintTeaa <rebloxminer@gmail.com>
2022-06-25 21:31:51 +0200

    Improved readability of IL

--- NoDust.cs
+++ NoDust.cs
@@ -15,22 +15,22 @@ namespace NoDust
 			var c = new ILCursor(il);
 			var if_end = c.DefineLabel();
 			var if_start = c.DefineLabel();
+
 			//Pushes value of config on the stack
 			c.EmitDelegate<Func<bool>>(() => {
 				return ModContent.GetInstance<NoDustConfig>().TurnOffDust;
 			});
 
-			c.MarkLabel(if_start);
+			// Jump to end of if statement if returned boolean value is false
+			c.Emit(OpCodes.Brfalse_S, if_end);
 
-			// Puts 6000 on the stack (this value, when returned from this method, means no dust is spawned)
+			// Puts 6000 on the stack and returns
+			// This value, when returned from this method, means no dust is spawned
 			c.Emit(OpCodes.Ldc_I4, 6000);
-
-			// Returns
 			c.Emit(OpCodes.Ret);
 
 			c.MarkLabel(if_end);
-			c.GotoLabel(if_start);
-			c.Emit(OpCodes.Brfalse_S, if_end);
+			
 		}
 	}
 }
